98d8e4a03eb5085de5735b2cb3d192c9702d6362
kaikeventura <kaike.gnr@hotmail.com>
2024-03-17 16:52:39 -0300

    Base para internacionalização

--- faker/src/main/kotlin/com/kaikeventura/kiafaker/json/reader/JsonReader.kt
+++ faker/src/main/kotlin/com/kaikeventura/kiafaker/json/reader/JsonReader.kt
@@ -1,10 +1,12 @@
 package com.kaikeventura.kiafaker.json.reader
 
 import com.google.gson.Gson
+import java.util.Locale
 
 class JsonReader {
 
     private val gson = Gson()
+    private val locale = Locale.getDefault()
 
     fun <T> read(clazz: Class<T>): T {
         val json = getJsonText(clazz.simpleName)
@@ -12,5 +14,5 @@ class JsonReader {
     }
 
     private fun getJsonText(clazzName: String): String =
-        this::class.java.classLoader.getResource("pt-br/${clazzName.lowercase()}.json")?.readText()!!
+        this::class.java.classLoader.getResource("$locale/${clazzName.lowercase()}.json")?.readText()!!
 }
