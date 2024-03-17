44c5554a7e0eac1d6ffc5cafb9399cb1820ece6d
kaikeventura <kaike.gnr@hotmail.com>
2024-03-17 11:30:56 -0300

    Criado módulos para separar responsabilidades

---
+++ core/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.kaikeventura"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
